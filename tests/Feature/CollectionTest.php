3e68f2c2fb7dce48d87fc42792e6429840e022ba
farhan wahyudi <farhanwahyudi875@gmail.com>
2024-11-17 09:00:57 +0700

    retrieve

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -308,4 +308,26 @@ class CollectionTest extends TestCase
         $this->assertEquals(array_values($result->all()[1]->all()), [5,6,7,8]);
         $this->assertEqualsCanonicalizing(array_values($result->all()[2]->all()), [9,10]);
     }
+
+    public function testFirst() {
+        $collection = collect([1,2,3,4,5,6,7,8,9]);
+        $result = $collection->first();
+        $this->assertEquals(1, $result);
+
+        $result = $collection->first(function ($value, $key) {
+            return $value > 5;
+        });
+        $this->assertEquals(6, $result);
+    }
+
+    public function testLast() {
+        $collection = collect([1,2,3,4,5,6,7,8,9]);
+        $result = $collection->last();
+        $this->assertEquals(9, $result);
+
+        $result = $collection->last(function ($value, $key) {
+            return $value < 5;
+        });
+        $this->assertEquals(4, $result);
+    }
 }
